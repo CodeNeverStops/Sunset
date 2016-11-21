313f5f0f95b6c22486d4328d6ca1959a6cb870b9
Wayne.You <lokiwuxi@gmail.com>
2016-11-21 10:58:33 +0800

    add static view

---
+++ app/src/main/java/io/github/uv_lab/sunset/SunsetFragment.java
@@ -0,0 +1,26 @@
+package io.github.uv_lab.sunset;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by youwei on 2016/11/21.
+ */
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+        return view;
+    }
+
+}
